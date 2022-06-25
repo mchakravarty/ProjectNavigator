6149e72fa044a8f3edfdeeb4ce552fd9dfd5185b
Manuel M T Chakravarty <chak@justtesting.org>
2022-06-25 21:02:48 +0200

    Bump iOS version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // Copyright 2022 Manuel M T Chakravarty
@@ -21,7 +21,7 @@ let package = Package(
   name: "ProjectNavigator",
   platforms: [
     .macOS(.v12),
-    .iOS(.v15)
+    .iOS(.v16)
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
