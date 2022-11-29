23a69f808a48811f7b6d2113a38afbe745885d46
Manuel M T Chakravarty <chak@justtesting.org>
2022-11-29 19:21:20 +0100

    Add DocC docs

--- Package.swift
+++ Package.swift
@@ -35,7 +35,10 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/apple/swift-collections.git",
-      .upToNextMajor(from: "1.0.0"))
+      .upToNextMajor(from: "1.0.0")),
+    .package(
+      url: "https://github.com/apple/swift-docc-plugin",
+      from: "1.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
